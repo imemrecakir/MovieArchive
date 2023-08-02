f23dd581558d651d526acc3098a1b13b1dd18688
Emre Çakır <imemrecakir@gmail.com>
2023-08-03 00:41:14 +0300

    Scroll View added to Detail View Controller

--- MovieArchive/Modules/Detail/DetailViewController.swift
+++ MovieArchive/Modules/Detail/DetailViewController.swift
@@ -18,10 +18,25 @@ class DetailViewController: UIViewController {
         }
     }
     
+    private let scrollView: UIScrollView = {
+       let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = true
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.isDirectionalLockEnabled = true
+        scrollView.contentInsetAdjustmentBehavior = .scrollableAxes
+        return scrollView
+    }()
+    
+    private let scrollContentView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     private let topContainerView: UIView = {
        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .clear
         return view
     }()
     
@@ -57,7 +72,7 @@ class DetailViewController: UIViewController {
         imageView.layer.borderColor = UIColor.secondaryLabel.cgColor
         imageView.layer.borderWidth = 0.75
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(systemName: "heart")?.withTintColor(.label, renderingMode: .alwaysOriginal)
+        imageView.image = UIImage(systemName: "bookmark")?.withTintColor(.label, renderingMode: .alwaysOriginal)
         imageView.backgroundColor = .secondarySystemBackground
         return imageView
     }()
@@ -76,23 +91,37 @@ class DetailViewController: UIViewController {
     
     private func setupUI() {
         view.backgroundColor = .secondarySystemBackground
+        navigationController?.navigationBar.isHidden = false
         viewModel.delegate = self
         viewModel.fetchMovieDetail(movieID: movieID)
-        navigationController?.navigationBar.isHidden = false
-        navigationItem.backButtonTitle = "Back"
+        
+        scrollView.addSubview(scrollContentView)
+        view.addSubview(scrollView)
         
         topContainerView.addSubview(backdropImageView)
         topContainerView.addSubview(posterImageView)
         topContainerView.addSubview(bookmarkImageView)
-        view.addSubview(topContainerView)
-        view.addSubview(infosContainerView)
+        scrollContentView.addSubview(topContainerView)
+        scrollContentView.addSubview(infosContainerView)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            topContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            topContainerView.topAnchor.constraint(equalTo: view.topAnchor),
-            topContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            scrollContentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            scrollContentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            scrollContentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            scrollContentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            scrollContentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            scrollContentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            
+            topContainerView.leadingAnchor.constraint(equalTo: scrollContentView.leadingAnchor),
+            topContainerView.topAnchor.constraint(equalTo: scrollContentView.topAnchor),
+            topContainerView.trailingAnchor.constraint(equalTo: scrollContentView.trailingAnchor),
             topContainerView.heightAnchor.constraint(equalToConstant: view.frame.height * 0.40),
             
             backdropImageView.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor),
@@ -101,23 +130,20 @@ class DetailViewController: UIViewController {
             backdropImageView.bottomAnchor.constraint(equalTo: topContainerView.bottomAnchor, constant: -(view.frame.height * 0.4) * 0.15),
             
             posterImageView.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor, constant: 32),
-            posterImageView.topAnchor.constraint(equalTo: topContainerView.topAnchor, constant: view.frame.height * 0.4 * 0.25),
-//            posterImageView.trailingAnchor.constraint(equalTo: bookmarkImageView.leadingAnchor, constant: -10),
+//            posterImageView.topAnchor.constraint(equalTo: topContainerView.topAnchor, constant: view.frame.height * 0.4 * 0.25),
             posterImageView.bottomAnchor.constraint(equalTo: topContainerView.bottomAnchor),
             posterImageView.widthAnchor.constraint(equalTo: topContainerView.widthAnchor, multiplier: 0.4),
+            posterImageView.heightAnchor.constraint(equalTo: topContainerView.heightAnchor, multiplier: 0.6f),
             
-            
-//            bookmarkImageView.topAnchor.constraint(equalTo: topContainerView.topAnchor),
             bookmarkImageView.trailingAnchor.constraint(equalTo: topContainerView.trailingAnchor, constant: -32),
             bookmarkImageView.bottomAnchor.constraint(equalTo: topContainerView.bottomAnchor, constant: -(view.frame.height * 0.4 * 0.05)),
-//            bookmarkImageView.leadingAnchor.constraint(equalTo: posterImageView.trailingAnchor),
             bookmarkImageView.heightAnchor.constraint(equalToConstant: 60),
             bookmarkImageView.widthAnchor.constraint(equalToConstant: 60),
             
-            infosContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            infosContainerView.leadingAnchor.constraint(equalTo: scrollContentView.leadingAnchor),
             infosContainerView.topAnchor.constraint(equalTo: topContainerView.bottomAnchor),
-            infosContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            infosContainerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            infosContainerView.trailingAnchor.constraint(equalTo: scrollContentView.trailingAnchor),
+            infosContainerView.bottomAnchor.constraint(equalTo: scrollContentView.bottomAnchor)
         ])
     }
 }
